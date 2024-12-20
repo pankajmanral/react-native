4de18f2ce206e8fe97cb32c38539e618d328c543
pankajmanral <pankajmanral0208@gmail.com>
2024-12-20 23:52:32 +0530

    started my app development journey

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  // const handlePress = () => {
+  //   console.log("The text has been clicked") 
+  // }
+
+  // const handleImagePress = () => {
+  //   console.log("The image has been pressed")
+  // }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text onPress={handlePress}>Pankaj here starting android development and he want to become a good full stack developer with both web and android</Text>
+      <TouchableHighlight onPress={handleImagePress}>
+        <Image 
+        blurRadius={4}
+        source={{
+          width : 200,
+          height : 300,
+          uri : 'https://picsum.photos/200/300'
+        }}/>
+      </TouchableHighlight> */}
+
+      <Button title="Click me" color="red" 
+        // onPress={() => Alert.alert("My alert", 
+        //                            "This is a custom alert", 
+        //                             [
+        //                               {text : "Yes", onPress : () => {console.log("Yes clicked")} },
+        //                               {text : 'No', onPress : ()=> {console.log("No pressed")} }
+        //                             ])}    
+
+        onPress = {
+          () => Alert.prompt("Question", "Enter your name" ,text => console.log(text))
+        }
+
+      ></Button>
+
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +46,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    alignItems:'center'
   },
 });
